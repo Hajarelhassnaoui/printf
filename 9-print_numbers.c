4acfb13d0ee447d7cd4f82c00bd6ac016d50b2fa
KAWTARBENSEUR <koukita0708kouki1000@gmail.com>
2023-07-25 19:23:08 -0700

    printf

--- 9-print_numbers.c
+++ 9-print_numbers.c
@@ -1,94 +1,38 @@
 #include "main.h"
 
 /**
- * _print_dec - prints decimal numbers
- * @valist: variable list of arguments
- * @f: pointer to flag_t
- * @m: pointer to mod_t
- * Return: Always (0);
- */
-int _print_dec(va_list valist, flag_t *f, mod_t *m)
-{
-	long lnum = va_arg(valist, long);
-	int count, num = 0;
-	short snum = 0;
-	int spaces, i;
-
-	if (m->l == 0 && m->h == 1)
-	{
-		snum = (short) lnum;
-		lnum = snum;
-	}
-	if (m->l == 0 && m->h == 0)
-	{
-		num = (int) lnum;
-		lnum = num;
-	}
-	count = count_digits(lnum);
-	if (m->width > count)
-	{
-		spaces = m->width - count;
-		for (i = 0; i < spaces; i++)
-			_putchar(' ');
-	}
-	if (f->space == 1 && f->plus == 0 && lnum >= 0)
-		count += _putchar(' ');
-	if (f->plus == 1 && lnum >= 0)
-		count += _putchar('+');
-	if (lnum <= 0)
-		count++;
-	print_number(lnum);
-	return (count);
-}
-
-
-/**
- * print_number - helps other functions print numbers to stdout
- * @n: int parameter
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: void
+ * Return: width.
  */
-void print_number(long n)
+int get_width(const char *format, int *i, va_list list)
 {
+	int curr_i;
+	int width = 0;
 
-	unsigned long m;
-
-	if (n < 0)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		_putchar('-');
-		m = -n;
-	}
-	else
-	{
-		m = n;
+		if (is_digit(format[curr_i]))
+		{
+			width *= 10;
+			width += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
 
-	if (m / 10)
-		print_number(m / 10);
-	_putchar('0' + (m % 10));
+	*i = curr_i - 1;
+
+	return (width);
 }
 
-
-/**
- * count_digits - counts the number of digits in a number
- * @n: int parameter
- *
- * Return: number of digits
- */
-int count_digits(long n)
-{
-	unsigned int count = 0;
-	unsigned long m;
-
-	if (n < 0)
-		m = (-1 * n);
-	else
-		m = n;
-	while (m != 0)
-	{
-		m /= 10;
-		count++;
-	}
-	return (count);
-}
 
